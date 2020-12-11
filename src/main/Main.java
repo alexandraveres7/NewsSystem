78c9e202441d462c4f84e4f292f1f950bdaeeb23
Darius <flocadariusandrei@gmail.com>
2020-12-11 13:15:14 +0200

    Finished up project (for real this time)

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,22 +3,28 @@ package main;
 import actors.Editor;
 import actors.Reader;
 
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         Reader reader1 = new Reader("Ale");
         Reader reader2 = new Reader("Ligia");
+        Reader reader3 = new Reader("Mihaela");
         Editor editor1 = new Editor("Darius Floca", "DailyMail");
         Editor editor2 = new Editor("Bence", "CNN");
 
+
         reader1.registerListener();
         reader2.registerListener();
+        reader3.registerListener();
 
-        reader1.addFilterCriteria("Date", "2019-08-08");
+
         reader1.addFilterCriteria("Source", "DailyMail");
         reader1.addFilterCriteria("Source", "CNN");
+
         reader2.addFilterCriteria("Domain", "shopping");
+
+        reader3.addFilterCriteria("Date", "2019-08-08");
+
+
         editor1.registerListener();
 
         editor1.publishArticle("Biden won", "The election has come to an end.", "politics", "USA");
